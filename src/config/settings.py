041617f4e34c452623cc503defa27da2fbf4fafc
ActualLearner <abdellahshafi7@gmail.com>
2025-09-27 20:50:43 +0300

    feat: enable JWT blacklist for enhanced security

--- src/config/settings.py
+++ src/config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
     "django_filters",
     "drf_spectacular",
     # My Apps
@@ -103,6 +105,8 @@ AUTH_USER_MODEL = "users.CustomUser"
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
 
 ROOT_URLCONF = "config.urls"
